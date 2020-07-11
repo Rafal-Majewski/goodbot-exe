25f9f5f59af8b3016e3a6d798d06fe970c228814
Rafał Majewski <goodheropl@gmail.com>
2020-07-11 12:19:55 +0200

    fix

--- actions/randomImage.js
+++ actions/randomImage.js
@@ -1,15 +1,16 @@
 const GoogleImages = require("google-images");
 
 module.exports={
-    trigger: (data)=>(Math.random() < 1),
+    trigger: (data)=>(Math.random() < 0.1),
     isOnlyForHumans: true,
     func: async(data)=>{
 		let words=data.message.content.split(" ").filter((word)=>(word)).map((word)=>(word.trim())).sort(()=>(Math.random()-0.5));
 		while (words.length != 0) {
 			let word=words.pop();
-			let images=await googleImagesClient.search(word);
-			if (images.length != 0) {
-				let image=images[Math.floor(Math.random()*images.length)];
+			let images=(await googleImagesClient.search(word)).sort(()=>(Math.random()-0.5));
+			while (images.length != 0) {
+				let image=images.pop();
+				if (!/[\/.](gif|jpg|bmp|jpeg|tiff|png)$/i.test(image.url)) continue;
 				return data.channel.send({
 					files: [image.url]
 				});
